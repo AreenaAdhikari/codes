e38f8f87411d1e1c567e65711bb4e39e9ee9ce11
AreenaAdhikari <adhikarijames6@gmail.com>
2025-02-16 10:39:04 +0000

    initial commit

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 # This program prints Hello, world!
 
 print('Hello, world!')
+# this program is printing a name
+print("My name is Areena")
+# this program code prints a sport name
+print("MY favourite sport is batminton")
+#this code script runs a sum
+print("the sum of 3+5 is:",3+5)
