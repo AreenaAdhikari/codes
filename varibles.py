0e8aa3d2ad85bfaef17192652f0a298b9e81b752
AreenaAdhikari <adhikarijames6@gmail.com>
2025-02-22 10:48:40 +0000

    initial commit

---
+++ varibles.py
@@ -0,0 +1,11 @@
+y = 5
+x = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+name = input("enter your name:")
+print(f"Debug: {name}")
+print("\nWelcome ",name ,"\nWelcome to codingal")
