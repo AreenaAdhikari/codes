91cde7d66115cae8928ad45e918a72bf798474af
AreenaAdhikari <adhikarijames6@gmail.com>
2025-03-02 10:47:35 +0000

    initial commit

---
+++ evenandoddnumber(s).py
@@ -0,0 +1,6 @@
+number = int(input("Enter the number to be checked :"))
+print("Number to be checkd is : ", number)
+if number%2==0 :
+    print("This is a even number")
+else:
+    print("this is an odd number")
