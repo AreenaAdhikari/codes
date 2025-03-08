611742d4dc755ade736052e7829f911cd1a86c2f
AreenaAdhikari <adhikarijames6@gmail.com>
2025-03-08 10:45:19 +0000

    initial commit

---
+++ notequalto.py
@@ -0,0 +1,18 @@
+a =10
+b =12
+c =12
+print(a != b)
+print (b != c)
+a = "python"
+b = "python"
+if a !=b:
+    print(a , 'and', b ,'are different.')
+
+a = 4
+b = 5
+if (a == b) != (b == 5):
+   print('Hello')
+
+a = int(input("enter a number"))
+if a%2 != 0 :
+    print("the number is not even")
