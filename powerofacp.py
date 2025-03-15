baa2383a4bb0e685e8ba5142396bd6ecd0579da2
AreenaAdhikari <adhikarijames6@gmail.com>
2025-03-15 10:48:47 +0000

    initial commit

---
+++ powerofacp.py
@@ -0,0 +1,4 @@
+number= float(input("Please enter a base number : "))
+exponent= int(input("Please enter a exponent number(n) :"))
+result = number**exponent
+print(f"the result of the base number {number} to the power off the exponent number {exponent} is {result}.:).")
