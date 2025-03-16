1c9d11756b159c98213ed28ce8cbb31c0e18f5b0
AreenaAdhikari <adhikarijames6@gmail.com>
2025-03-16 10:47:02 +0000

    initial commit

--- armstrongnumber.py
+++ armstrongnumber.py
@@ -1,10 +1,12 @@
 num = int(input("please Enter a number : "))
 temp = num
+am=0
 while temp >0:
-    digit= temp % 10
-    sum += digit ** 3
-    temp //=10
-if num == sum:
+    digit = temp % 10
+    am += digit ** 3
+    temp //= 10
+
+if num == am:
     print(num, "is an Armstrong number")
 else:
     print(num, "is not an Armstrong number")
