2aee133be0c05bbdcfa32464826dd15a3e605166
AreenaAdhikari <adhikarijames6@gmail.com>
2025-04-06 10:53:33 +0100

    initial commit

---
+++ Shutdown.py
@@ -0,0 +1,8 @@
+def shutdown():
+    print("Saving all the works....")
+    print("Closing all the applications....")
+    print("Logging out the users...")
+    print("The System is shutting down...Please wait...")
+    print("Goodbye!")
+
+shutdown()
