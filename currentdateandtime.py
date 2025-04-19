865d8f7d9cc1db313cf40a3ac16c9b27cc6c140e
AreenaAdhikari <adhikarijames6@gmail.com>
2025-04-19 11:22:35 +0100

    initial commit

---
+++ currentdateandtime.py
@@ -0,0 +1,6 @@
+from datetime import date,time, datetime
+today = date.today()
+now = datetime.today()
+print("TOdays time is",today)
+print("\nCurrent time is ", now)
+print("date time components are" , today.year , today.month , today.day )
