4c86567ef983d6ce44e30932c824a212beb737f1
AreenaAdhikari <adhikarijames6@gmail.com>
2025-04-26 11:21:49 +0100

    initial commit

---
+++ tuplesinpython.py
@@ -0,0 +1,11 @@
+tuplex = ("tuple", False,3.2,1)
+print(tuplex)
+tuplex =(2,3,4,55,7,8,9,0)
+print(tuplex)
+tuplex = tuplex + (9, )
+print(tuplex)
+tuplex1 = (2,4,3,5,4,6,7,8,4,1)
+_slice = tuplex1[3:2]
+print(_slice)
+_slice = tuplex1[:6]
+print(_slice)
