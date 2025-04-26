2bc478043984f9d22788303ccc4c7aa7cd485502
AreenaAdhikari <adhikarijames6@gmail.com>
2025-04-26 11:38:27 +0100

    initial commit

--- tuplesinpython.py
+++ tuplesinpython.py
@@ -5,7 +5,7 @@ print(tuplex)
 tuplex = tuplex + (9, )
 print(tuplex)
 tuplex1 = (2,4,3,5,4,6,7,8,4,1)
-_slice = tuplex1[3:2]
+_slice = tuplex1[3:5]
 print(_slice)
 _slice = tuplex1[:6]
 print(_slice)
