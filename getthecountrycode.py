f19939b99f77594c2b560a7b69efcf30966a08ec
AreenaAdhikari <adhikarijames6@gmail.com>
2025-04-27 11:45:11 +0100

    initial commit

---
+++ getthecountrycode.py
@@ -0,0 +1,7 @@
+SCPMcountry_code = {'India': '0091',
+                    'Australia': '0027',
+                    'Nepal' : '0977'}
+print("Country code of India -")
+print(SCPMcountry_code.get('India','Not Found'))
+print("Country code of Japan -")
+print(SCPMcountry_code.get('Japan','Not Found'))
