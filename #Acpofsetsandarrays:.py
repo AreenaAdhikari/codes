9b32c3be3c91d64fc1021f73abf7f22ced402c3a
AreenaAdhikari <adhikarijames6@gmail.com>
2025-05-10 09:06:50 +0100

    initial commit

---
+++ #Acpofsetsandarrays:.py
@@ -0,0 +1,19 @@
+#Acpofsetsandarrays:
+print("Original set 1: ")
+set1x = {1,2,4,5,63,6,87,90,}
+print(set1x)
+print("Original set2")
+setx2 = {1,2,3,44,55,76,8,9,90}
+print(setx2)
+print("Intersection:")
+a =set1x.intersection(setx2)
+print(a)
+print("Union:")
+Bb = set1x.union(setx2)
+print(Bb)
+print("Differance:")
+ccbc = set1x.difference(setx2)
+print(ccbc)
+print("Symmetric Differance: ")
+defc = set1x.symmetric_difference(setx2)
+print(defc)
