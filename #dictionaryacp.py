ded7ba2e295743c5ba5799e29c76a40a0f3ec1a1
AreenaAdhikari <adhikarijames6@gmail.com>
2025-05-10 08:47:31 +0100

    initial commit

---
+++ #dictionaryacp.py
@@ -0,0 +1,28 @@
+#dictionaryacp
+car_data={'id1':
+   {'name':['car1'] ,  
+     'color': ['blue'],
+     'general': ['toyota,speed maxium 5000mph']  
+   },
+   'id2':
+   {'name:': ['yotacar2'],
+    'color': ['red'],
+    'general': ['toyota,speed maxium 400mph'],
+    },
+    'id3':
+   {'name:': ['yotacartoôooo'],
+    'color': ['redish and yelloish gradient'],
+    'general': ['toyota and volswagon mix hybrid car ,speed maxium 1000mph'],
+    },
+     'id3':
+   {'name:': ['shAURAYAUV'],
+    'color': ['redish and yelloish gradient WITH A MIX OF BLUE'],
+    'general': ['Tesla ,speed maxium 1000mph'],
+    },
+       }
+result = {}
+for key,value in car_data.items():
+    if value not in result.values():
+        result[key] = value
+
+print(result)
