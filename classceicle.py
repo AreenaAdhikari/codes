ad5a02cab90b9b39e7a01d72d70b2a372b069bc6
AreenaAdhikari <adhikarijames6@gmail.com>
2025-05-24 11:25:46 +0100

    initial commit

---
+++ classceicle.py
@@ -0,0 +1,7 @@
+class vechicle:
+    def __init__(self,max_speed,mileage):
+      self.max_speed = max_speed
+      self.mileage = mileage
+mode1x = vechicle(240,18)
+print("Model max speed :",mode1x.max_speed)
+print("Model mileage :",mode1x.mileage)
