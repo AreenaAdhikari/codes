315c65d456e21be80e94bd13d63b5e1a4a8f13a0
AreenaAdhikari <adhikarijames6@gmail.com>
2025-07-20 11:22:06 +0100

    initial commit

---
+++ toplevelwindow.py
@@ -0,0 +1,16 @@
+from tkinter import *
+root = Tk()
+root.geometry("400x500")
+root.title("main")
+def topwin():
+    top = Toplevel()
+    top.geometry("180x100")
+    top.title("Toplevel")
+    l2 = Label(top, text="THis is a top level window")
+    l2.pack()
+    top.mainloop()
+l = Label(root, text="This is the main window")
+btn = Button(root,text="Click here to open another window", command= topwin)
+l.pack()
+btn.pack()
+root.mainloop()
